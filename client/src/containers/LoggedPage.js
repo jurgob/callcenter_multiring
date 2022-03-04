0af15f616bd190e1ac1de44a13b9a304f84ec75c
jurgob <jurgo.boemo@gmail.com>
2022-03-04 15:42:25 +0000

    remove unused code

--- client/src/containers/LoggedPage.js
+++ client/src/containers/LoggedPage.js
@@ -44,7 +44,7 @@ function LoggedPage(props) {
     //init cs client, called on login success
     useEffect(() => {
 
-        
+
         if(token && token === props.loginData?.token){
             return;
         }else{
@@ -90,143 +90,6 @@ function LoggedPage(props) {
         initCSClient()
 
     }, [props.loginData?.token])
-
-    // useEffect(() => {
-    //     console.log(` ->->->-> useEffect csClient Handler `, csClient);
-    //     if (!csClient)
-    //         return;
-
-    //     const onTrack = (e) => {
-    //         const _setAudioSrcObject = audioState.peerConnection.getRemoteStreams()[0]
-    //         console.log(`_setAudioSrcObject `, _setAudioSrcObject)
-
-    //         setAudioState(as => {
-    //             console.log(`setAudioState onTrack`, {
-    //                 ...as,
-    //                 audioSrcObject: _setAudioSrcObject
-    //             })
-    //             return {
-    //                 ...as,
-    //                 audioSrcObject: _setAudioSrcObject
-    //             }
-    //         })
-    //     }
-
-    //     csClient.onEvent(async (evt) => {
-    //         if (evt.type === 'rtc:answer') {
-    //             const sdp = evt.body.answer
-    //             const remoteDescription = new RTCSessionDescription({
-    //                 type: 'answer',
-    //                 sdp,
-    //             })
-    //             if (audioState.peerConnection) {
-    //                 audioState.peerConnection.ontrack = onTrack
-    //                 audioState.peerConnection.setRemoteDescription(remoteDescription)
-    //             }
-    //         }
-    //     })
-
-    //     csClient.onRequestEnd(async (event) => {
-    //         const url = event.request.url
-    //         if (url.includes("/users/") && url.includes("/conversations") ) {
-    //             const conversations = event.response.data._embedded.conversations
-    //             setMyConversationsState(conversations)
-    //         }
-
-            
-    //     })
-
-    // }, [audioState])
-
-    // const onEnableAudioInConversationSubmit = async (data) => {
-    //     // const { conversation_id } = data
-    //     try {
-    //         console.log(`--- onEnableAudioInConversationSubmit`, data)
-    //         const { audio_conversation_id } = data
-    //         const conversation_id = audio_conversation_id
-    //         const pc = await createRtcAudioConnection()
-
-    //         console.log(`setAudioState `, { ...audioState, peerConnection: pc })
-    //         setAudioState({ ...audioState, peerConnection: pc })
-
-    //         // peerConnection = pc
-    //         const userConvRes = await csClient.request({
-    //             url: `/v0.3/users/${csClient.getSessionData().user_id}/conversations`,
-    //             method: "get"
-    //         })
-
-    //         const member_id = userConvRes.data._embedded.conversations.find(({ id }) => id === conversation_id)._embedded.member.id
-
-    //         await csClient.request({
-    //             url: `/v0.1/conversations/${conversation_id}/rtc`,
-    //             method: "post",
-    //             data: {
-    //                 from: member_id,
-    //                 body: {
-    //                     offer: pc.localDescription,
-    //                 }
-    //             }
-    //         })
-
-    //     } catch (e) {
-    //         console.log(`onEnableAudioInConversationSubmit error: `, e)
-    //     }
-
-    // }
-
-    // const onCreateConversationSubmit = async (data) => {
-    //     const { conversation_name, conversation_display_name } = data
-    //     const convRes = await csClient.request({
-    //         url: `/v0.3/conversations`,
-    //         method: "post",
-    //         data: {
-    //             "name": conversation_name,
-    //             "display_name": conversation_display_name
-    //         }
-    //     })
-
-    //     await csClient.request({
-    //         url: `/v0.3/conversations/${convRes.data.id}/members`,
-    //         method: "post",
-    //         data: {
-    //             "state": "joined",
-    //             "user": {
-    //                 name: csClient.getSessionData().user_name,
-    //             },
-    //             "channel": {
-    //                 "type": "app"
-    //             }
-    //         }
-    //     })
-
-    // }
-
-    // const onJoinConversationSubmit = async (data) => {
-    //     console.log('onJoinConversationSubmit ', data)
-    //     const { conversation_join_id} = data
-    //     await csClient.request({
-    //         url: `/v0.3/conversations/${conversation_join_id}/members`,
-    //         method: "post",
-    //         data: {
-    //             "state": "joined",
-    //             "user": {
-    //                 name: csClient.getSessionData().user_name,
-    //             },
-    //             "channel": {
-    //                 "type": "app"
-    //             }
-    //         }
-    //     })
-
-    // }
-
-    // const getMyConversations = async () => {
-    //     await csClient.request({
-    //         url: `/v0.3/users/${csClient.getSessionData().user_id}/conversations`,
-    //         method: "get"
-    //     })
-    // }
-
     
     return (
         <div className="App">
@@ -237,31 +100,7 @@ function LoggedPage(props) {
                     direction={curCall.direction} 
                     onReject={() => { curCall.reject()  }} 
                 />}
-                {/* <h2>Create Conversation and Join</h2>
-                <FormCreateConversation onSubmit={onCreateConversationSubmit} />
-                <h2>Join Conversation</h2>
-                <FormJoinConversation onSubmit={onJoinConversationSubmit} />
-                <h2>Get My Conversations</h2>
-                <button onClick={getMyConversations} >Get My Conversations</button>
-                <h2>Enable Audio In Conversations</h2>
-                <FormEnableAudioInConversations onSubmit={onEnableAudioInConversationSubmit} />
-                <Audio srcObject={audioState.audioSrcObject} />
-                <div>
-                    Conversations ({myConversationsState.length})
-                    {myConversationsState.length && <div>
-                        {myConversationsState.map(({ name, id, _embedded}) => {
-                            const { member } = _embedded
-                            return (<div key={id}>
-                                <b>{name}: </b>{id} - {member.state} - {member.id}
-                            </div>)
-                        })}
-                        
-
-                    </div>
-
-                    }
-                </div> */}
-
+               
 
                 <EventsHistory
                     eventsHistory={eventsHistory}
